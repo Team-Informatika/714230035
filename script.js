b3979ee2d7c5f256e5494de7258725d8bfc97171
Kim Dokja <memskie324@gmail.com>
2025-05-09 21:46:58 +0700

    p

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-// Scroll effect for active navbar link highlight
-const navbarLinks = document.querySelectorAll('.navbar a');
-navbarLinks.forEach(link => {
-  link.addEventListener('click', function () {
-    navbarLinks.forEach(link => link.classList.remove('text-yellow-500'));  // Remove active class from all links
-    this.classList.add('text-yellow-500');  // Add active class to clicked link
-  });
-});
-
-// Toggle navbar for mobile view
-const toggleBtn = document.querySelector('.navbar-toggle');
-const navbarMenu = document.querySelector('.navbar-menu');
+// Toggle the navigation menu on mobile
+const toggleBtn = document.getElementById('navbar-toggle');
+const navbarMenu = document.querySelector('.navbar .space-x-6');
 
 toggleBtn.addEventListener('click', () => {
   navbarMenu.classList.toggle('hidden');
